ef8161280b6bec8709c26baecb324839404d273e
MORI <34857747+MORI233@users.noreply.github.com>
2023-12-04 15:20:29 +0900

    Update prepare_data.py

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -14,7 +14,6 @@ SERIES_SCHEMA = {
     "step": pl.UInt32,
     "anglez": pl.Float32,
     "enmo": pl.Float32,
-    "enmo_1v_30m_mean": pl.Float32,
 }
 
 
@@ -30,7 +29,6 @@ FEATURE_NAMES = [
     "minute_cos",
     "anglez_sin",
     "anglez_cos",
-    "enmo_1v_30m_mean",
 ]
 
 ANGLEZ_MEAN = -8.810476
